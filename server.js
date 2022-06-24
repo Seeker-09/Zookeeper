031ef95910c108e156752740d2f1aed6d4ad4acc
Seeker <54086931+Seeker-09@users.noreply.github.com>
2022-06-24 14:37:52 -0400

    actually finished for srue this time

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-const { application } = require('express');
 const express = require('express');
-const {animals} = require('./data/animals.json');
-const fs = require('fs');
-const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = process.env.PORT || 3001
-const app = express();
-
-// parse incoming string or array data
-app.use(express.urlencoded({extended: true}));
-// parse incoming JSON data
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + "/public"));
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use(express.static('public'));
+
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}`);
-})
+  console.log(`API server now on port ${PORT}!`);
+});
